ff9bd64fb3a2f0417bfe59e99d1935c0d07ae466
Tomasz Kostur <tomasz.kostur@gmail.com>
2026-06-20 15:44:46 +0200

    Add some dynamic in root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-import { Fragment } from "react";
-import { Outlet, ScrollRestoration } from "react-router-dom";
+// import { Fragment } from "react";
+// import { Outlet, ScrollRestoration } from "react-router-dom";
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <Fragment>
+//       <Outlet />
+//       <ScrollRestoration />
+//     </Fragment>
+//   );
+// }
+const gymTopics = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
   return (
-    <Fragment>
-      <Outlet />
-      <ScrollRestoration />
-    </Fragment>
+    <header>
+      <img src="src/assets/gym.png" alt="Gym logo" />
+      <h1>React Remind gym</h1>
+      <p>
+        Lets for once do not care about styling and use one from example.
+      </p>
+    </header>
   );
 }
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h2>Todays gym topic is: {gymTopics[genRandomInt(2)]}. Do {genRandomInt(100)} number of sets</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
