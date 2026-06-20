ba98860fe3a218542a94989c597ac47751f0d583
Tomasz Kostur <tomasz.kostur@gmail.com>
2026-06-20 20:32:29 +0200

    Buttons changes dynamic content

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@
 //     </Fragment>
 //   );
 // }
-
+import {SetStateAction, useState} from "react";
 import gymImg from "./assets/gym.png";
 import benchPressImg from "./assets/02_bench_press.svg";
 import deadliftImg from "./assets/03_deadlift.svg";
@@ -21,6 +21,12 @@ function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
+const description_map = {
+  bench: "good for chest",
+  deadlift: "good for spine",
+  squat: "good_for legs",
+};
+
 function Header() {
   return (
     <header>
@@ -31,17 +37,27 @@ function Header() {
   );
 }
 
-function ExcersiseTab(props) {
+function ExcersiseTab({ img, name, level, onClickHandler}) {
   return (
     <li>
-      <img src={props.img} alt={props.name} class="icon-white" />
-      <h3>{props.name}</h3>
-      <p>{props.level}</p>
+      <img src={img} alt={name} class="icon-white" />
+      <h3>{name}</h3>
+      <p>{level}</p>
+      <button
+        onClick={onClickHandler}
+      >
+        Do it
+      </button>
     </li>
   );
 }
 
 function App() {
+  const [current_excercise, current_excercise_setter] = useState('bench');
+  function description_change(new_excercise){
+    current_excercise_setter(new_excercise);
+  }
+
   return (
     <div>
       <Header />
@@ -52,10 +68,26 @@ function App() {
         </h2>
         <section id="core-concepts">
           <ul>
-            <ExcersiseTab img={benchPressImg} name="Bench" level="100" />
-            <ExcersiseTab img={deadliftImg} name="Dead Lift" level="180" />
-            <ExcersiseTab img={barbelSquatImg} name="Squat" level="34" />
+            <ExcersiseTab
+              img={benchPressImg}
+              name="Bench"
+              level="100"
+              onClickHandler={()=>{description_change('bench')}}
+            />
+            <ExcersiseTab
+              img={deadliftImg}
+              name="Dead Lift"
+              level="180"
+              onClickHandler={()=>{description_change('deadlift')}}
+            />
+            <ExcersiseTab
+              img={barbelSquatImg}
+              name="Squat"
+              level="34"
+              onClickHandler={()=>{description_change('squat')}}
+            />
           </ul>
+          <p>{description_map[current_excercise]}</p>
         </section>
       </main>
     </div>
