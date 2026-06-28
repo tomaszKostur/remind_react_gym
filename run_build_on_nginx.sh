37dfa9a556945b617e2dbed765a90381e0b8aba9
Tomasz Kostur <tomasz.kostur@gmail.com>
2026-06-28 20:33:03 +0200

    Add landing page, to remove later

--- run_build_on_nginx.sh
+++ run_build_on_nginx.sh
@@ -1,9 +1,15 @@
 pnpm build
 # cp -r ./dist/* /usr/share/nginx/html/
 
+# the react_demo app
+rm -rf /var/www/html/*
 mkdir -p /var/www/html/react_demo
-rm -rf /var/www/html/react_demo/*
 cp -r ./dist/* /var/www/html/react_demo
+
+# landing_page
+cp ./landing_page/index.html /var/www/html/index.html
+cp ./landing_page/style_landing_page.css /var/www/html/style_landing_page.css
+
 nginx -s quit
 nginx
 # nginx -s quit # to gracefully stop nginx
